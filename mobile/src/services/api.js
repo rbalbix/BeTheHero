ba3962a61bf78ffd563fe785e71a909d4648e65b
Ricardo Balbi <rbalbi@gmail.com>
2020-04-09 12:49:16 -0300

    revert(283a1c0): 283a1c0

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-// baseURL: 'http://192.168.1.107:3333';
-
 const api = axios.create({
-  baseURL: 'https://rb-behero.herokuapp.com'
+  baseURL: 'http://192.168.1.107:3333'
 });
 
 export default api;
